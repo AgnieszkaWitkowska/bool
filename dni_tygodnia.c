8b3e17358b05889d98e59309faeb30f4fc97ae7f
agawit <agnieszkakojro@wp.pl>
2018-10-12 09:32:11 +0200

    dni tygodnia

--- dni_tygodnia.c
+++ dni_tygodnia.c
@@ -1,10 +1,15 @@
+#include <stdbool.h>
 #include "stdio.h"
 int main(){
     enum dni {
-        ni, po, wt, sr, cz, pi, so
+        ni, po=5, wt, sr, cz, pi, so
     };
     enum dni dzien_tygodnia;
     dzien_tygodnia = wt;
-    printf("%i", dzien_tygodnia);
+    bool czy_byl_obecny = true;
+
+    printf("%i\n", czy_byl_obecny);
+
+
 
 }
