70c78f6f0a9550f3014fab03423fcc1d4044f565
agawit <agnieszkakojro@wp.pl>
2018-10-12 10:08:04 +0200

    dni tygodnia 3

--- dni_tygodnia.c
+++ dni_tygodnia.c
@@ -1,12 +1,12 @@
 #include <stdbool.h>
 #include "stdio.h"
 int main(){
-    enum dni {
-        ni, po=5, wt, sr, cz, pi, so
-    };
-    enum dni dzien_tygodnia;
+    typedef enum { // typdef sluzy do nazywania typu (typ dni)
+        ni, po=5, wt, sr, cz, pi, so// mamy możliwosc wpływu na indeksowanie elementow typu wyliczeniowego
+    } dni;
+    dni dzien_tygodnia;
     dzien_tygodnia = wt;
-    bool czy_byl_obecny = true;
+    bool czy_byl_obecny = false;
 
     printf("%i\n", czy_byl_obecny);
 
