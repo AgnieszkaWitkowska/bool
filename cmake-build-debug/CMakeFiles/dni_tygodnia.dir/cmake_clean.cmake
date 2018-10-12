57538749f958d0d5a24b1cac593873733b41913a
agawit <agnieszkakojro@wp.pl>
2018-10-12 09:42:35 +0200

    dni tygodnia

---
+++ cmake-build-debug/CMakeFiles/dni_tygodnia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dni_tygodnia.dir/dni_tygodnia.c.o"
+  "dni_tygodnia.pdb"
+  "dni_tygodnia"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dni_tygodnia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
